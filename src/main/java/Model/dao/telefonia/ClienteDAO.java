68cc60195344b23cf9856370aede44e06c70b39b
Victor Saccucci <victorsaccucci@hotmail.com>
2023-04-01 16:16:50 -0300

    Métodos de verificar cpf e inserir clientes criados.

--- src/main/java/Model/dao/telefonia/ClienteDAO.java
+++ src/main/java/Model/dao/telefonia/ClienteDAO.java
@@ -9,6 +9,52 @@ import Model.vo.telefonia.Cliente;
 import Model.vo.telefonia.Endereco;
 
 public class ClienteDAO {
+	
+	public Cliente inserir(Cliente novoCliente) {
+		Connection conexao = Banco.getConnection();
+		String sql = " INSERT INTO CLIENTE(NOME, CPF, ID_ENDERECO, ATIVO) "
+				+ " VALUES (?,?,?,?) ";
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setString(1, novoCliente.getNome());
+			stmt.setString(2, novoCliente.getCpf());
+			stmt.setInt(3, novoCliente.getEndereco().getId());
+			stmt.setBoolean(4, novoCliente.isAtivo());
+			//stmt.setDate(5, java.sql.Date.valueOf(novoCliente.getDataNascimento()));
+			stmt.execute();
+			
+			ResultSet resultado = stmt.getGeneratedKeys();
+			if(resultado.next()) {
+				novoCliente.setId(resultado.getInt(1));
+			}
+		}catch(SQLException e) {
+			System.out.println("Erro ao inserir novo cliente!" + 
+					"\nCausa: " + e.getMessage());
+		}
+		return novoCliente;
+	}
+	
+	public boolean cpfJaUtilizado(String cpfBuscado) {
+		boolean cpfJaUtilizado = false;
+		Connection conexao = Banco.getConnection();
+		String sql = "SELECT COUNT(*) FROM CLIENTE WHERE CPF = ?";
+		
+		PreparedStatement query = Banco.getPreparedStatement(conexao, sql);
+		try {
+			query.setString(1, cpfBuscado);
+			ResultSet resultado = query.executeQuery();
+			
+			if(resultado.next()) {
+				cpfJaUtilizado = resultado.getInt(1) > 0;
+			}
+		}catch(SQLException e) {
+			System.out.println("Erro ao verificar uso do CPF " + cpfBuscado 
+					+ "\n Causa:" + e.getMessage());
+		}
+		return cpfJaUtilizado;
+	}
+	
 	public Cliente consultarPorId(int id) {
 		Cliente clienteBuscado = null;
 		Connection conexao = Banco.getConnection();
